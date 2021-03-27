a74fa8f1c3424ab046f4138124b85ec0c783a2e2
jhonnierandrey <jhonnierandrey@outlook.com>
2021-03-27 02:01:59 -0300

    New proto for user managing and test with the API

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,10 +2,47 @@ import React, { Component } from 'react';
 import nameLogo from '../images/reactBank.png';
 
 class Welcome extends Component {
+
+    state = {
+        msg: "",
+        userData: []
+    }
+
+    usernameInput = React.createRef();
+    passwordInput = React.createRef();
     
     showLogin(){
         document.querySelector(".login-form").style.display = 'flex';
         document.querySelector(".App-header").style.display = 'none';
+    }
+
+    getData = (e) => {
+        e.preventDefault();
+        
+        const username = this.usernameInput.current.value;
+        const password = this.passwordInput.current.value;
+
+        this.callapi(username, password)
+    }
+
+    callapi(username, password){
+        const url = "https://reactbank-back-end.herokuapp.com/api/login/";
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: username,
+                password: password,
+            })
+            }
+        )
+        .then(response => response.json())
+        .then(result => this.setState({msg : result.msg}))
+        .then(console.log(this.state.msg))
     }
 
     render() {
@@ -15,9 +52,9 @@ class Welcome extends Component {
                 <div className="login-form">
                     <img src={nameLogo} alt='Logo'/>
                     
-                    <form>
-                        <input type="text" placeholder='username'/>
-                        <input type="password" placeholder='password'/>
+                    <form onSubmit={this.getData}>
+                        <input ref={this.usernameInput} type="email" placeholder='username' name="username" required/>
+                        <input ref={this.passwordInput} type="password" placeholder='password' name="password" required/>
 
                         <div>
                             <button type="submit" className="btn btn-primary">LOGIN</button>
