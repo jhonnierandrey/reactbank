e2dac8a96f4e629f476a2fb3197f58c60f85e2e8
jhonnierandrey <jhonnierandrey@outlook.com>
2021-03-28 16:24:00 -0300

    App.js contains all components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,99 @@
-import './App.css';
-import Home from './components/Home';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <Home />
-  );
+import './App.css';
+import nameLogo from './images/reactBank.png';
+
+import { Apply, About, Dashboard, Login, Modal} from "./components/index";
+
+class App extends Component {
+
+    state = {
+        msg: "",
+        userData: []
+    }
+    
+    showLogin(e){
+        e.preventDefault();
+
+        document.querySelector(".apply-form").style.display = 'none';
+        document.querySelector(".login-form").style.display = 'flex';
+        document.querySelector(".App-header").style.display = 'none';
+        document.querySelector(".about").style.display = 'none';
+    }
+
+    showApply(e){
+        e.preventDefault();
+
+        document.querySelector(".login-form").style.display = 'none';
+        document.querySelector(".App-header").style.display = 'none';
+        document.querySelector(".apply-form").style.display = 'flex';
+        document.querySelector(".about").style.display = 'none';
+    }
+
+    showAbout(e){
+        e.preventDefault();
+
+        document.querySelector(".login-form").style.display = 'none';
+        document.querySelector(".App-header").style.display = 'none';
+        document.querySelector(".apply-form").style.display = 'none';
+        document.querySelector(".about").style.display = 'flex';
+    }
+
+    showModal(e){
+        e.preventDefault();
+
+        document.querySelector(".modal-container").style.display = 'flex';
+        // document.querySelector(".login-form").style.display = 'none';
+    }
+
+    showDashboard(e){
+      e.preventDefault();
+
+      document.querySelector(".login-form").style.display = 'none';
+      document.querySelector(".App-header").style.display = 'none';
+      document.querySelector(".apply-form").style.display = 'none';
+      document.querySelector(".about").style.display = 'none';
+      document.querySelector(".dashboard-container").style.display = 'initial';
+    }
+
+    render() {
+
+        return (
+            <div className="App">
+                <Login
+                  showApply = {this.showApply}
+                  showAbout = {this.showAbout}
+                />
+
+                <Apply
+                    showLogin = {this.showLogin}
+                    showAbout = {this.showAbout}
+                />
+
+                <About
+                    showLogin = {this.showLogin}
+                    showApply = {this.showApply}
+                />
+
+                <Dashboard />
+
+                <header className="App-header">
+                    <img src={nameLogo} alt='Logo'/>
+                    <h3>welcome back.</h3>
+                    
+                    <button onClick={this.showLogin} type="button" className="btn btn-primary">Login</button>
+                    <button onClick={this.showDashboard} type="button" className="btn btn-primary">TEST</button>
+                </header>
+
+                <div className='screen-alert'>
+                    <img src={nameLogo} alt='Logo'/>
+                    <h3>will be available soon on Desktop.</h3>        
+                </div>
+
+                <Modal />
+            </div>
+            );
+        }
 }
 
 export default App;
