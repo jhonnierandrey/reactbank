a2824e587622153ad4e1552e91c9eff02795559d
jhonnierandrey <jhonnierandrey@outlook.com>
2021-03-28 02:43:17 -0300

    App.js code simplified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-//import logo from './logo.svg';
 import './App.css';
-import Welcome from './components/Welcome';
+import Home from './components/Home';
 
 function App() {
-  // condition
-  // 
   return (
-    <Welcome />
+    <Home />
   );
 }
 
