fd77cb70e877d9b243c2707039285930c63b1a25
jhonnierandrey <jhonnierandrey@outlook.com>
2021-03-28 16:23:04 -0300

    Re-structuring app UI

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Apply} from './Apply';
+export {default as About} from './About';
+export {default as Login} from './Login';
+export {default as Modal} from './Modal';
+export {default as Dashboard} from './Dashboard';
