8316bcbf2f20b1dd2017f7e1561cd36c604a5bac
jhonnierandrey <jhonnierandrey@outlook.com>
2021-03-30 20:15:40 -0300

    functions move apart

--- src/App.js
+++ src/App.js
@@ -1,119 +1,69 @@
 import React, { Component } from 'react';
-import Cookies from 'universal-cookie';
+// import Cookies from 'universal-cookie'; will be implemented soon.
 
 import './App.css';
 import nameLogo from './images/reactBank.png';
 
 import { Apply, About, Dashboard, Login, Modal} from "./components/index";
 
+// custom functions to modify viewport.
+import show from './scripts'
+
 class App extends Component {
 
     state = {
-        username : '',
+        token : '',
         msg: '',
-        userData: [],
-    }
-    
-    showLogin = (e) => {
-        e.preventDefault();
-
-        document.querySelector(".apply-form").style.display = 'none';
-        document.querySelector(".login-form").style.display = 'flex';
-        document.querySelector(".App-header").style.display = 'none';
-        document.querySelector(".about").style.display = 'none';
-    }
-
-    showApply = (e) => {
-        e.preventDefault();
-
-        document.querySelector(".login-form").style.display = 'none';
-        document.querySelector(".App-header").style.display = 'none';
-        document.querySelector(".apply-form").style.display = 'flex';
-        document.querySelector(".about").style.display = 'none';
-    }
-
-    showAbout = (e) => {
-        e.preventDefault();
-
-        document.querySelector(".login-form").style.display = 'none';
-        document.querySelector(".App-header").style.display = 'none';
-        document.querySelector(".apply-form").style.display = 'none';
-        document.querySelector(".about").style.display = 'flex';
-    }
-
-    showModal = (e) =>{
-        e.preventDefault();
-
-        document.querySelector(".modal-container").style.display = 'flex';
-        // document.querySelector(".login-form").style.display = 'none';
-    }
-
-    showDashboard = (e) => {
-      e.preventDefault();
-
-      document.querySelector(".login-form").style.display = 'none';
-      document.querySelector(".App-header").style.display = 'none';
-      document.querySelector(".apply-form").style.display = 'none';
-      document.querySelector(".about").style.display = 'none';
-      document.querySelector(".dashboard-container").style.display = 'initial';
+        userData: '',
     }
 
     callapi = (username, password) => {
         document.querySelector(".modal-container").style.display = 'flex';
 
         const url = "https://reactbank-back-end.herokuapp.com/api/login/";
-
-        this.setState(
-            {username : username}
-        )
+        // localStorage.setItem("token", "value")
 
         fetch(url, {
             method: 'POST',
-            credentials: "include",
+            credentials: "same-origin",
             headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/json; charset=utf-8',
             },
             body: JSON.stringify({
                 email: username,
                 password: password,
             })
-            }
-        )
+        })
         .then(response => response.json())
-        .then(result => this.setState({ msg : result.msg }))
+        .then(result => this.setState({ msg : result.msg, token : result.token }))
         .then(this.verifyLogin)
     }
-
+    
     verifyLogin = () => {
         if(this.state.msg === 'User Logged In'){
+            // future-feature 
+            // const cookies = new Cookies();
+            // cookies.set('currentUser', this.state.username, {path: '/'});
+            
             // get user data
-            console.log("Information is correct.")
-
-            const cookies = new Cookies();
-            cookies.set('currentUser', this.state.username, {path: '/'});
-            // window.location.href='./dashboard';
-
-            console.log(cookies.get('currentUser'))
-
             const url = "https://reactbank-back-end.herokuapp.com/api/account";
-
-            fetch(url,{
+            
+            fetch(url, {
                 method: 'GET',
-                credentials: "include",
+                credentials: "same-origin",
                 headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
+                    'Content-Type': 'application/json; charset=utf-8',
+                    'token' : this.state.token
+                }
             })
             .then(response => response.json())
-            .then(result => console.log(result))
-            // .then(result => this.setState({images : result.hits}))
-            // .then(this.scrollDown) 
-            // remove modal
-            // move to dashboard
+            .then(result => this.setState({ userData : result.userData }))
+            .then(show.dashboard)
         }else{
-            console.log("Information is incorrect.")
+            // hide modal & show login screen + errors
+            document.querySelector(".modal-container").style.display = 'none';
+            document.querySelector(".form-errors").style.display = 'initial';
+            document.querySelector(".form-errors").innerHTML = this.state.msg;
         }
     }
 
@@ -123,28 +73,29 @@ class App extends Component {
             <div className="App">
                 <Login
                     callapi = {this.callapi}
-                    showApply = {this.showApply}
-                    showAbout = {this.showAbout}
+                    showApply = {show.apply}
+                    showAbout = {show.about}
                 />
 
                 <Apply
-                    showLogin = {this.showLogin}
-                    showAbout = {this.showAbout}
+                    showLogin = {show.login}
+                    showAbout = {show.about}
                 />
 
                 <About
-                    showLogin = {this.showLogin}
-                    showApply = {this.showApply}
+                    showLogin = {show.login}
+                    showApply = {show.apply}
                 />
 
-                <Dashboard />
+                <Dashboard
+                    userData = {this.state.userData}
+                />
 
                 <header className="App-header">
                     <img src={nameLogo} alt='Logo'/>
                     <h3>welcome back.</h3>
                     
-                    <button onClick={this.showLogin} type="button" className="btn btn-primary">Login</button>
-                    <button onClick={this.showDashboard} type="button" className="btn btn-primary">TEST</button>
+                    <button onClick={show.login} type="button" className="btn btn-primary">Login</button>
                 </header>
 
                 <div className='screen-alert'>
