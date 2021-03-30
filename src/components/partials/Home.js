8d641e673f62bdc527e48e18c5792e3c3e2a2d4d
jhonnierandrey <jhonnierandrey@outlook.com>
2021-03-30 20:18:29 -0300

    Login runs faster and secured

--- src/components/partials/Home.js
+++ src/components/partials/Home.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Home() {
-    return(
-        <section className="home">
-            <article className="available-balance">
-                <h1>$1,588.66</h1>
-            </article>
-            <article className="actions">
-                <button type="button" className="btn btn-outline-success"><i className="fas fa-arrow-circle-down"></i> Deposit</button>
-                <button type="button" className="btn btn-outline-danger"><i className="fas fa-arrow-circle-up"></i> Withdrawal</button>
-            </article>
-            <hr />
-            <article className="transactions">
-                <ul>
-                    <li>No recent transactions.</li>
-                </ul>
-            </article>
-        </section>
-    )
+class Home extends Component {
+
+    render() {
+        return(
+            <section className="home">
+                <article className="available-balance">
+                    <h1>$<span>{this.props.accountData.available}</span></h1>
+                </article>
+                <article className="actions">
+                    <button type="button" className="btn btn-outline-success"><i className="fas fa-arrow-circle-down"></i> Deposit</button>
+                    <button type="button" className="btn btn-outline-danger"><i className="fas fa-arrow-circle-up"></i> Withdrawal</button>
+                </article>
+                <hr />
+                <article className="transactions">
+                    <ul>
+                        <li>No recent transactions.</li>
+                    </ul>
+                </article>
+            </section>
+        )
+    }
 }
 
 export default Home;
