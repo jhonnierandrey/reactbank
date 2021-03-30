6afff2a8e4ccb34f845995db0b3069d097551c7f
jhonnierandrey <jhonnierandrey@outlook.com>
2021-03-30 00:09:53 -0300

    Linking back with front

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Cookies from 'universal-cookie';
 
 import './App.css';
 import nameLogo from './images/reactBank.png';
@@ -8,11 +9,12 @@ import { Apply, About, Dashboard, Login, Modal} from "./components/index";
 class App extends Component {
 
     state = {
-        msg: "",
-        userData: []
+        username : '',
+        msg: '',
+        userData: [],
     }
     
-    showLogin(e){
+    showLogin = (e) => {
         e.preventDefault();
 
         document.querySelector(".apply-form").style.display = 'none';
@@ -21,7 +23,7 @@ class App extends Component {
         document.querySelector(".about").style.display = 'none';
     }
 
-    showApply(e){
+    showApply = (e) => {
         e.preventDefault();
 
         document.querySelector(".login-form").style.display = 'none';
@@ -30,7 +32,7 @@ class App extends Component {
         document.querySelector(".about").style.display = 'none';
     }
 
-    showAbout(e){
+    showAbout = (e) => {
         e.preventDefault();
 
         document.querySelector(".login-form").style.display = 'none';
@@ -39,14 +41,14 @@ class App extends Component {
         document.querySelector(".about").style.display = 'flex';
     }
 
-    showModal(e){
+    showModal = (e) =>{
         e.preventDefault();
 
         document.querySelector(".modal-container").style.display = 'flex';
         // document.querySelector(".login-form").style.display = 'none';
     }
 
-    showDashboard(e){
+    showDashboard = (e) => {
       e.preventDefault();
 
       document.querySelector(".login-form").style.display = 'none';
@@ -56,13 +58,73 @@ class App extends Component {
       document.querySelector(".dashboard-container").style.display = 'initial';
     }
 
+    callapi = (username, password) => {
+        document.querySelector(".modal-container").style.display = 'flex';
+
+        const url = "https://reactbank-back-end.herokuapp.com/api/login/";
+
+        this.setState(
+            {username : username}
+        )
+
+        fetch(url, {
+            method: 'POST',
+            credentials: "include",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: username,
+                password: password,
+            })
+            }
+        )
+        .then(response => response.json())
+        .then(result => this.setState({ msg : result.msg }))
+        .then(this.verifyLogin)
+    }
+
+    verifyLogin = () => {
+        if(this.state.msg === 'User Logged In'){
+            // get user data
+            console.log("Information is correct.")
+
+            const cookies = new Cookies();
+            cookies.set('currentUser', this.state.username, {path: '/'});
+            // window.location.href='./dashboard';
+
+            console.log(cookies.get('currentUser'))
+
+            const url = "https://reactbank-back-end.herokuapp.com/api/account";
+
+            fetch(url,{
+                method: 'GET',
+                credentials: "include",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then(response => response.json())
+            .then(result => console.log(result))
+            // .then(result => this.setState({images : result.hits}))
+            // .then(this.scrollDown) 
+            // remove modal
+            // move to dashboard
+        }else{
+            console.log("Information is incorrect.")
+        }
+    }
+
     render() {
 
         return (
             <div className="App">
                 <Login
-                  showApply = {this.showApply}
-                  showAbout = {this.showAbout}
+                    callapi = {this.callapi}
+                    showApply = {this.showApply}
+                    showAbout = {this.showAbout}
                 />
 
                 <Apply
