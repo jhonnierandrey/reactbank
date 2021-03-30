9f7c6efae7f8e63628b861805e247feb715c5607
jhonnierandrey <jhonnierandrey@outlook.com>
2021-03-30 20:17:56 -0300

    Using dinamic information

--- src/components/partials/Account.js
+++ src/components/partials/Account.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Account() {
-    return(
-        <section className="account">
-            <article className="account-information">
-                <h1>Jen Smith</h1>
-                <h5>jen@smith.com</h5>
-                <h5>$1,588.66</h5>
-            </article>
-            <article className="actions">
-                <button type="button" className="btn btn-outline-success"><i className="fas fa-user-edit"></i> Update</button>
-                <button type="button" className="btn btn-outline-danger"><i className="fas fa-sign-out-alt"></i> Log out</button>
-            </article>
-            <hr />
-            <article className="account-information-update">
-                <p>First name: <span>Jen</span></p>
-                <p>Last name: <span>Smith</span></p>
-                <p>e-mail: <span>jen@smith.com</span></p>
-            </article>
-        </section>
-    )
+class Account extends Component {
+
+    render() {
+        return(
+            <section className="account">
+                <article className="account-information">
+                    <h1><span>{this.props.userData.name}</span> <span>{this.props.userData.lastName}</span></h1>
+                    <h5><span>{this.props.userData.email}</span></h5>
+                    <h5>$<span>{this.props.userData.accountData?.available}</span></h5>
+                </article>
+                <article className="actions">
+                    <button type="button" className="btn btn-outline-success"><i className="fas fa-user-edit"></i> Update</button>
+                    <button type="button" className="btn btn-outline-danger"><i className="fas fa-sign-out-alt"></i> Log out</button>
+                </article>
+                <hr />
+                <article className="account-information-update">
+                    <p>First name: <span>Jen</span></p>
+                    <p>Last name: <span>Smith</span></p>
+                    <p>e-mail: <span>jen@smith.com</span></p>
+                </article>
+            </section>
+        )
+    }
 }
 
 export default Account;
